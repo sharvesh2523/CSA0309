18010a95e6ba3007741b0b04818c910530b8c9f1
sharvesh2523 <ssreesharvesh@gmail.com>
2025-09-08 17:42:49 +0530

    exp2.c

--- exp2.c
+++ exp2.c
@@ -1,1 +1,8 @@
-
+#include <stdio.h>
+int main(){
+    int n,i;
+    printf("How many numbers? "); scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    for(i=0;i<n;i++) printf("%d is %s\n",a[i],a[i]%2?"Odd":"Even");
+}
