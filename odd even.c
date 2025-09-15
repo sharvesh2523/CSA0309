fb7fa256699b051899be708536631e7503509458
sharvesh2523 <ssreesharvesh@gmail.com>
2025-09-15 12:50:13 +0530

    Create odd even.c

---
+++ odd even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[50], n, i;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    printf("Even numbers: ");
+    for (i = 0; i < n; i++) if (arr[i] % 2 == 0) printf("%d ", arr[i]);
+
+    printf("\nOdd numbers: ");
+    for (i = 0; i < n; i++) if (arr[i] % 2 != 0) printf("%d ", arr[i]);
+
+    return 0;
+}
