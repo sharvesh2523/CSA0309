e57fb75864afd11b1be849375f29e59fee5e8e86
sharvesh2523 <ssreesharvesh@gmail.com>
2025-09-15 12:48:10 +0530

    Create linked list.c

---
+++ linked list.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* mergeLists(struct Node* a, struct Node* b) {
+    if (!a) return b;
+    if (!b) return a;
+    if (a->data < b->data) {
+        a->next = mergeLists(a->next, b);
+        return a;
+    } else {
+        b->next = mergeLists(a, b->next);
+        return b;
+    }
+}
+
+void printList(struct Node* head) {
+    while (head) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* list1 = createNode(1);
+    list1->next = createNode(3);
+    list1->next->next = createNode(5);
+
+    struct Node* list2 = createNode(2);
+    list2->next = createNode(4);
+    list2->next->next = createNode(6);
+
+    struct Node* merged = mergeLists(list1, list2);
+    printf("Merged List: ");
+    printList(merged);
+    return 0;
+}
