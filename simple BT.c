a6af7a51cb6925d54e1241ad29a02f98c203f844
sharvesh2523 <ssreesharvesh@gmail.com>
2025-09-16 09:32:07 +0530

    Create simple BT.c

---
+++ simple BT.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void preorder(struct Node* node) {
+    if(node != NULL) {
+        printf("%d ", node->data);
+        preorder(node->left);
+        preorder(node->right);
+    }
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Preorder traversal:\n");
+    preorder(root);
+    printf("\n");
+
+    return 0;
+}
